bfa2c54a4e82f4093449417d200e308d0a4e0017
user-coder07 <prashanthraos8867@gmail.com>
2025-03-19 15:43:53 -0800

    added to the branch

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+package z;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int product = a * b;
+        
+        System.out.println("Product: " + product);
+    }
+}
